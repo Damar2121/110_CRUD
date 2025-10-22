385fc7a85554110e975879ad1af96605e28e2889
Damar2121 <damar.vivoy51@gmail.com>
2025-10-22 11:25:27 +0700

    memperbaiki codingan error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-let mysql = require("mysql12");
+let mysql = require("mysql2");
 const app = express();
 const PORT = 3001;
 app.use(express.json());
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '200421D@m2121',
   database: 'mahasiswa',
   PORT: 3306
 });
